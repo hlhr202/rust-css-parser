ba56e1f87f9018408a3006ae00f33495c05d372e
haoran <hlhr202@163.com>
2021-10-15 02:41:07 +0800

    rename

--- index.js
+++ index.js
@@ -1,60 +1,8 @@
-import { lex } from "./pkg/rust_css_parser_test.js";
+import fs from "fs";
+import { parse } from "./pkg/rust_css_parser_test.js";
 
-const source = `@variable: #999;
-
-@variable2: #fff;
-
-@import url();
-
-@media only screen and (max-width: 1000px) {
-    color: white;
-    font-size: 10px;
-}
-
-main {
-    * {
-        font-size: 3em;
-    }
-    color: #999;
-    -webkit-line-clamp: 3;
-    h3 {
-        font-size: large;
-        background: url("https://www.baidu.com");
-    }
-    div + p {
-        list-style: "|";
-    }
-    .test-1, #test2 {
-        --theme-color: var(--white);
-    }
-    [data-attr] {
-        background-color: white;
-        left: calc(100% - 10px);
-    }
-    &.img {
-        width: fit-content;
-    }
-    & {
-        div {
-            overflow: initial;
-        }
-    }
-    #what {
-        right: 0;
-    }
-}
-
-.test-class1 {
-    color: white;
-}
-
-@keyframes anim {
-    from {
-        left: 0%;
-    }
-    to {
-        right: 100%;
-    }
-}`;
-
-console.log(lex(source));
+fs.promises
+    .readFile("./test/test.less", { encoding: "utf-8" })
+    .then((source) => {
+        console.log(parse(source));
+    });
--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ mod parser;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn lex(source: &str) -> JsValue {
+pub fn parse(source: &str) -> JsValue {
     let tokens = lexer::Lexer::new().lex_from_source(&source.to_owned());
     let mut parser = parser::Parser::new(&tokens);
     let nodes = parser.parse();
